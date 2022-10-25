cbd5f070ae5870be6ef6b245c1248175440c7b3c
Kyle Monk <kmonk@es99.agency>
2022-10-25 10:19:19 -0500

    TextBox Test

---
+++ src/stories/TextBox/TextBox.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10%;
+  width: 50vw;
+  height: 50vh;
+  background-color: ${(props) => props.color};
+`;
+
+export const TextBox = ({color}) => {
+  return (
+    <Box color={color}>
+      <h2>Text Box</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Box>
+  );
+};
+
+export default TextBox;
---
+++ src/stories/TextBox/TextBox.stories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import {TextBox} from "./TextBox";
+
+export default {
+  title: "Personal/TextBox",
+  component: TextBox,
+  argTypes: {},
+};
+
+const Template = (args) => <TextBox {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  color: "#333333",
+};
